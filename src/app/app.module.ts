ea23fa5fd61038db364e7886e2ddd5ee17fd210f
mahieurobindev <94044404+mahieurobindev@users.noreply.github.com>
2022-04-30 08:33:52 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MotoFormComponent } from './moto/components/moto-form/moto-form.component';
 import { MotoDetailsComponent } from './moto/pages/moto-details/moto-details.component';
 import { MotoListComponent } from './moto/pages/moto-list/moto-list.component';
+import { Moto } from './moto./moto..component';
+import { MotoComponent } from './moto/moto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MotoFormComponent,
     MotoDetailsComponent,
-    MotoListComponent
+    MotoListComponent,
+    Moto.Component,
+    MotoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/moto/moto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moto',
+  templateUrl: './moto.component.html',
+  styleUrls: ['./moto.component.scss']
+})
+export class MotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
