3b564fddcc0127fcffb0d9a000aa41f99160e5f4
mahieurobindev <94044404+mahieurobindev@users.noreply.github.com>
2022-04-30 08:44:30 +0200

    add

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule
