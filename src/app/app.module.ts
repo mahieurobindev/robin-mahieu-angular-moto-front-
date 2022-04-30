75d2a6f62c57eb19bc5f9ae84cdb9402ab388bad
mahieurobindev <94044404+mahieurobindev@users.noreply.github.com>
2022-04-30 08:51:47 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MotoFormComponent } from './moto/components/moto-form/moto-form.component';
-import { MotoDetailsComponent } from './moto/pages/moto-details/moto-details.component';
-import { MotoListComponent } from './moto/pages/moto-list/moto-list.component';
-import { Moto } from './moto./moto..component';
-import { MotoComponent } from './moto/moto.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MotoFormComponent,
-    MotoDetailsComponent,
-    MotoListComponent,
-    Moto.Component,
-    MotoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     BrowserAnimationsModule
   ],
   providers: [],
